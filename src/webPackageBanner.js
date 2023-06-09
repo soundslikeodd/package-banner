92cf0aa0716f810373e71a3242897bcfaf9ba4a2
soundlikeodd <mattschaadt@gmail.com>
2023-06-08 20:04:45 -0700

    test: unit test web api

--- src/webPackageBanner.js
+++ src/webPackageBanner.js
@@ -8,7 +8,11 @@ const webPackageBanner = config => {
         packageNameFont
     } = config;
     const fontName = packageNameFont || figletOptions?.font || 'Standard';
-    if (figletFontFileData) figlet.parseFont(fontName, figletFontFileData);
+    if (!figletFontFileData) {
+        console.error('No figlet font file data present. figletFontFileData is required for web usage.');
+        return;
+    }
+    figlet.parseFont(fontName, figletFontFileData);
     figlet.loadFont([fontName], () => {
         packageBanner(config);
     });
